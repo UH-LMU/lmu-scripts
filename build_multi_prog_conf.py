f352bca863a818ddfa8c10dcfd086620f37be5db
Harri Jäälinoja <harri.jaalinoja@helsinki.fi>
2013-04-02 12:56:02 +0300

    add support for continuing runs

--- build_multi_prog_conf.py
+++ build_multi_prog_conf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from optparse import OptionParser
 import os
+import string
 import subprocess
 
 usage = ''
@@ -8,27 +9,39 @@ usage = ''
 def main():
     parser = OptionParser(usage=usage)
     parser.add_option('--prog', help="")
-    parser.add_option('--data-dir', help="")
+    #parser.add_option('--data-dir', help="")
     parser.add_option('--data', help="")
     parser.add_option('--masks-dir', default="masks",  help="")
     parser.add_option('--masks', default="mask", help="")
     parser.add_option('--results-dir', default="results", help="")
     parser.add_option('--results', default="results",  help="")
-    parser.add_option('--processors', help="")
+    parser.add_option('--processors', default=1, help="")
+    parser.add_option('--rerun', default=0, help="")
 
     options, args = parser.parse_args()
 
     prog = options.prog
-    data_dir = options.data_dir
+    #data_dir = options.data_dir
     data = options.data
     masks_dir = options.masks_dir
     masks = options.masks
     results_dir = options.results_dir
     results = options.results
     processors = int(options.processors)
+    rerun = int(options.rerun)
+    
+    
     
     for i in range(0, processors):
-        line = "%d     %s --save_spots %s/%s%d.txt --log_ratios %s/%s%d.bmp %s" % (i,  prog, results_dir,  results,  i, masks_dir,  masks,  i,  data )
+        save_spots = "--save_spots %s/%s%d-%d.txt" % (results_dir,  results,  i, rerun)
+        mask = "--log_ratios %s/%s%d.bmp" % (masks_dir,  masks,  i)
+        load_checkpoint = ""
+        if rerun:
+            previous = string.replace(save_spots, '-'+str(rerun),  '-'+str(rerun -1))
+            load_checkpoint = "--load_checkpoint " + previous
+            
+        line = "%d     %s %s %s %s %s" % (i,  prog, load_checkpoint,  save_spots,  mask,  data)
+
         print line
         
 if __name__ == "__main__":
