3a6b83126443621f09c2ca3c69ad7af75827b29b
Harri Jäälinoja <harri.jaalinoja at helsinki.fi>
2014-07-07 16:28:29 +0300

    debugged

--- aggregate_plate_data.py
+++ aggregate_plate_data.py
@@ -9,16 +9,18 @@ from sqlalchemy.ext.automap import automap_base
 
 db = "/mnt/lmu-active/LMU-active1/users/joseph/CellProfiler/output/DefaultDB.db"
 
+# http://stackoverflow.com/questions/14180866/sum-each-value-in-a-list-of-tuples
 def sums(rows):
     return map(sum,izip(*rows))
 
 def means(rows):
     return map(numpy.mean,izip(*rows))
 
+
 class WellPlate96:
     ROWS = ['A','B','C','D','E','F','G','H']
     COLS = ['01','02','03','04','05','06','07','08','09','10','11','12']
-    cols12 = ',,,,,,,,,,,\n'
+    cols12 = ',,,,,,,,,,,,\n'
 
     def __init__(self,name,headers,data):
         self.name = name
@@ -30,13 +32,41 @@ class WellPlate96:
             self.sums[well] = sums(data[well])
             self.means[well] = means(data[well])
 
+    def _add_aggregate(self,output,aggregate,label,i):
+        output = output + '"%s %s %s",,,,,,,,,,\n'%(self.name,label,self.headers[i])
+        # add column labels
+        output = output + ','
+        for c in self.COLS:
+            output = output + '"%s",'%c
+        output = output + '\n'
+
+        for r in self.ROWS:
+            output = output + '"%s",' % r
+            values = []
+            for c in self.COLS:
+                well = r+c
+                if aggregate.has_key(well):
+                    value = aggregate[well][i]
+                else:
+                    value = ""
+                values.append(value)
+            output = output + '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n'% tuple(values)
+        return output
+    
+
     def printout(self):
-##        output = '"%s",,,,,,,,,,,\n'%self.name
-##        output = output + cols12
-##        
+        output = ''
+        #output = '"%s",,,,,,,,,,,\n'%self.name
+        #output = output + self.cols12
+        
         for h in self.headers:
-            print str(h)
-        print self.sums['A01']
+            i = self.headers.index(h)
+            output = self._add_aggregate(output,self.sums,"SUM",i)
+            output = output + self.cols12
+            output = self._add_aggregate(output,self.means,"MEAN",i)
+            output = output + self.cols12
+
+        print output
 
         
 
@@ -63,15 +93,13 @@ if __name__ == '__main__':
                    image.Image_Intensity_MeanIntensity_Green]
     
     # find unique plate/well combinations
-    plates_and_wells = session.query(plate,well).group_by(plate,well).all()
     plates = session.query(distinct(plate)).all()
+    #print plates
     
-    #for pw in plates_and_wells:
-        #results = session.query(*data).filter(plate==pw[0],well==pw[1]).all()
     for p in plates:
-        print p
+        #print "Plate %s, looking for wells..." % p[0]
         wells = session.query(distinct(well)).filter(plate==p[0]).all()
-        print p,wells
+        #print p[0],wells
 
         data = {}
         for w in wells:
@@ -79,10 +107,11 @@ if __name__ == '__main__':
             results = session.query(*dataheaders).filter(plate==p[0],well==w[0]).all()
             #print p,w,results
             data[w[0]] = results
+        #print data
 
-        plate = WellPlate96(p[0],dataheaders,data)
-        plate.printout()
-        sys.exit()
+        wellplate = WellPlate96(p[0],dataheaders,data)
+        wellplate.printout()
+        #sys.exit()
                                           
         
 
