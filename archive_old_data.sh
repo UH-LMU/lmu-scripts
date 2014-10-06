9634b2de553c2c41c0061174abbd4077ac2ed3f3
Harri Jäälinoja <harri.jaalinoja at helsinki.fi>
2014-10-06 17:47:16 +0300

    working on it

--- archive_old_data.sh
+++ archive_old_data.sh
@@ -43,6 +43,10 @@ active=`echo $1 | sed 's#/$##'`
 archive=`echo $2 | sed 's#/$##'`
 
 # check inputs
+if [ -z $active ]; then
+	usage
+fi
+
 if [ ! -d "$active" ]; then
 	echo "$active" is not a directory.
 	exit
@@ -68,23 +72,67 @@ timestamp=${OPT_TIMESTAMP:-`date +%Y%m%d%H%M`}
 
 transferlist="$active"/transfer_`basename "$active"`_"$timestamp".txt
 logfile="$active"/archive_`basename "$active"`_"$timestamp".log
+echo 
 echo transferlist: "$transferlist"
 echo logfile: "$logfile"
+echo
 
 # find files that are older than a year
 #find "$active" -type f -mtime +365 > "$transferlist"
+find "$active" -type f -mtime -365 > "$transferlist"
 
 # edit file list so that paths start in the directory to be archived
 sed -i "s#^.*$active##" "$transferlist"
 
 # rsync files to archive
-cmd="rsync -rv --files-from=$transferlist"
+rsync=`which rsync`
+cmd="$rsync -rv --files-from=$transferlist"
 if [ -n "$OPT_DRY_RUN" ]; then
 	cmd="$cmd --dry-run"
 fi
 if [ -n "$OPT_REMOVE_SOURCE_FILES" ]; then
-	cmd="$cmd --remove-source-files"
+	echo using cmd="$cmd --remove-source-files"
 fi
-cmd="$cmd $active $archive >& $logfile"
-echo "$cmd"
-xargs "$cmd"
+cmd="$cmd $active $archive"
+echo rsync command: "$cmd >& $logfile"
+echo
+eval $cmd >& $logfile
+
+
+split_logs() {
+	logfile=$1
+	inputdir=$2/$3
+	inputsubdir=$3
+	echo
+	echo split_logs: $logfile $inputdir $inputsubdir
+
+	# http://stackoverflow.com/questions/301039/how-can-i-escape-white-space-in-a-bash-loop-list
+	while IFS= read -r -d '' subdir; do
+		# use subdirectory name for log file naming
+		base=`basename "$subdir"`
+		sublog=$subdir/`basename $logfile .log`_$base.log
+
+		# we want to grep from the start of the line,
+		# so if logs are split for in a named subdirectory,
+		# add the input subdirectory name to the search string
+		if [ -n $inputsubdir ]; then
+			base=$inputsubdir/$base
+		fi
+
+		echo "grep ^$base $logfile >& $sublog"
+		if [ -z "$OPT_DRY_RUN" ]; then
+			grep "^$base" "$logfile" >& "$sublog"
+		fi
+
+    	done < <(find $inputdir -mindepth 1 -maxdepth 1 -type d -print0)
+}
+
+
+# split log file per subdirectories of the input folder
+split_logs $logfile $active
+
+# split log file per subsubdirectories of the input folder
+if [ -n $opt_LOG_SPLIT_ROOT ]; then
+	split_logs $logfile $active $OPT_LOG_SPLIT_ROOT
+fi
+
