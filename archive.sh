8d643b5d1187ac881ce835b2aa6d9298055f485a
Harri Tapio Jaalinoja <hajaalin@csc-lmu-ubuntu.biocenter.helsinki.fi>
2014-10-22 12:51:40 +0300

    script that is run on csc-lmu

---
+++ archive.sh
@@ -0,0 +1,3 @@
+./archive_old_data.sh -d -l LMU-active1 /mnt/lmu-active-rw/LMU-active1/users/ /mnt/lmu-archive-rw/LMU-archive1/Users/
+./archive_old_data.sh -d -l LMU-active2 /mnt/lmu-active-rw/LMU-active2/users/ /mnt/lmu-archive-rw/LMU-archive2/users/
+
