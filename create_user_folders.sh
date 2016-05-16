8787f78fc30e1eb81b071aeb9fa0d031a2cad322
Harri Jäälinoja <harri.jaalinoja at helsinki.fi>
2016-05-16 15:43:47 +0300

    add folders on active and archive 3

--- create_user_folders.sh
+++ create_user_folders.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bases=("/mnt/lmu-active/LMU-active1/users" "/mnt/lmu-active/LMU-active2/users" "/mnt/lmu-archive/LMU-archive1/users" "/mnt/lmu-archive/LMU-archive2/users")
+bases=("/mnt/lmu-active/LMU-active1/users" "/mnt/lmu-active/LMU-active2/users" "/mnt/lmu-active/LMU-active3/users" "/mnt/lmu-archive/LMU-archive1/users" "/mnt/lmu-archive/LMU-archive2/users" "/mnt/lmu-archive/LMU-archive3/users")
 
 users=$(ldapsearch -H ldaps://ldap-internal.it.helsinki.fi:636 -x -s sub -b ou=alma_workgroups,ou=groups,o=hy "(&(uid=grp-A91900-lmu-cust))" \
 |grep uniqueMember|cut -d' ' -f2 |cut -d, -f1|cut -d'=' -f2|tr '[:upper:]' '[:lower:]'|sort)
