caf525dc26599dfbd50f26e34ab93d7046ce5770
Harri Jäälinoja <harri.jaalinoja@helsinki.fi>
2016-09-08 12:02:10 +0300

    add new network drives

--- create_user_folders.sh
+++ create_user_folders.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-bases=("/home/hajaalin/netapp/lmu/users" "/mnt/lmu-active/LMU-active1/users" "/mnt/lmu-active/LMU-active2/users" "/mnt/lmu-active/LMU-active3/users" "/mnt/lmu-archive/LMU-archive1/users" "/mnt/lmu-archive/LMU-archive2/users" "/mnt/lmu-archive/LMU-archive3/users")
+active=("/mnt/lmu-active/LMU-active1/users" "/mnt/lmu-active/LMU-active2/users" "/mnt/lmu-active/LMU-active3/users")
+archive=("/mnt/lmu-archive/LMU-archive1/users" "/mnt/lmu-archive/LMU-archive2/users" "/mnt/lmu-archive/LMU-archive3/users")
+netapp=("/mnt/lmu-netapp/users")
+mednas=("/mnt/med-groups/lmu/ls1/users" "/mnt/med-groups/lmu/ls2/users" "/mnt/med-groups/lmu/lmu_archive/users")
+
+bases=("${active[@]}" "${archive[@]}" "${netapp[@]}" "${mednas[@]}")
+#bases=("/home/hajaalin/netapp/lmu/users" "/mnt/lmu-active/LMU-active1/users" "/mnt/lmu-active/LMU-active2/users" "/mnt/lmu-active/LMU-active3/users" "/mnt/lmu-archive/LMU-archive1/users" "/mnt/lmu-archive/LMU-archive2/users" "/mnt/lmu-archive/LMU-archive3/users")
 
 users=$(ldapsearch -H ldaps://ldap-internal.it.helsinki.fi:636 -x -s sub -b ou=alma_workgroups,ou=groups,o=hy "(&(uid=grp-A91900-lmu-cust))" \
 |grep uniqueMember|cut -d' ' -f2 |cut -d, -f1|cut -d'=' -f2|tr '[:upper:]' '[:lower:]'|sort)
