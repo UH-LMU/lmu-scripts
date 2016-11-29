85db7853fb7259614c869a2912c0babead781596
Harri Jäälinoja <harri.jaalinoja@helsinki.fi>
2016-11-29 10:31:18 +0200

    change mount points

--- archive.sh
+++ archive.sh
@@ -1,3 +1,3 @@
-./archive_old_data.sh -d -l LMU-active1 /mnt/lmu-active-rw/LMU-active1/users/ /mnt/lmu-archive-rw/LMU-archive1/Users/
-./archive_old_data.sh -d -l LMU-active2 /mnt/lmu-active-rw/LMU-active2/users/ /mnt/lmu-archive-rw/LMU-archive2/users/
+./archive_old_data.sh -d -l LMU-active1 /mnt/lmu-active/LMU-active1/users/ /mnt/lmu-archive/LMU-archive1/Users/
+./archive_old_data.sh -d -l LMU-active2 /mnt/lmu-active/LMU-active2/users/ /mnt/lmu-archive/LMU-archive2/users/
 
