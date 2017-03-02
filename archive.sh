f80efe93757edad3e338f19516fcba843bdafb41
Harri Jäälinoja <harri.jaalinoja@helsinki.fi>
2017-03-02 15:33:07 +0200

    moved scripts to internal repo

--- archive.sh
+++
@@ -1,3 +0,0 @@
-./archive_old_data.sh -d -l LMU-active1 /mnt/lmu-active/LMU-active1/users/ /mnt/lmu-archive/LMU-archive1/Users/
-./archive_old_data.sh -d -l LMU-active2 /mnt/lmu-active/LMU-active2/users/ /mnt/lmu-archive/LMU-archive2/users/
-
